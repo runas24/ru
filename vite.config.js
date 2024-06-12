50a0246dc53063cbd95e55a678bdc6aad062d541
Runas24 <runnnasss4@gmail.com>
2024-06-12 12:50:38 +0500

    56757567

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue()],
-+  base: '/ru/'
-})
+  plugins: [vue()],
+  base: '/ru', // замените на имя вашего репозитория
+});
